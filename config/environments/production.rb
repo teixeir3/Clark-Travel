ce96cd0c5fda7a94523a04ecb84fe2c57f1459fd
Douglas Teixeira <teixeir3@gmail.com>
2014-10-30 15:46:56 -0400

    Changes production.rb to serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ ClarkTravel::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -77,15 +77,15 @@ ClarkTravel::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
   config.action_mailer.default_url_options = { :host => 'clarktravel.com' }
-  
-  config.paperclip_defaults = {                                   
+
+  config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }                                                             
-  }  
+    }
+  }
 end
