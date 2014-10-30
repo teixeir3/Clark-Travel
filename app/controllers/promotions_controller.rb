61b9cf4fd25d0f46ff6daf17a6deaf5398d3b76f
Douglas Teixeira <teixeir3@gmail.com>
2014-10-30 16:23:21 -0400

    Moves favicon to public folder. Adds require_signed_in! before_filter to PromotionsController

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,16 +1,18 @@
 class PromotionsController < ApplicationController
-  
+  before_filter :require_signed_in!,
+    only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @promotions = Promotion.all_active
   end
-  
+
   def new
     @promotion = current_user.promotions.new
   end
-  
+
   def create
     @promotion = current_user.promotions.build(permitted_params.promotion)
-    
+
     if @promotion.save
       flash.now[:notices] = ["Promotion created!"]
       render :show
@@ -19,21 +21,21 @@ class PromotionsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @promotion = Promotion.find(params[:id])
   end
-  
+
   def edit
     @promotion = Promotion.find(params[:id])
   end
-  
+
   def update
     @promotion = Promotion.find(params[:id])
   end
-  
+
   def destroy
     @promotion = Promotion.find(params[:id])
   end
-  
+
 end
