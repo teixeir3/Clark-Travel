b3ec7b42562c46a4176b3ae58da9ea5a9d678dde
Douglas Teixeira <teixeir3@gmail.com>
2014-11-07 00:16:37 -0500

    Fixes error in oauth migration.

--- db/migrate/20141107050739_add_facebook_oauth_to_user.rb
+++ db/migrate/20141107050739_add_facebook_oauth_to_user.rb
@@ -1,8 +1,6 @@
 class AddFacebookOauthToUser < ActiveRecord::Migration
   def change
-    add_column :user, :provider, :string
-    add_column :user, :uid, :string
-    add_column :user, :oauth_token, :string
-    add_column :user, :oauth_expires_at, :datetime
+    add_column :users, :oauth_token, :string
+    add_column :users, :oauth_expires_at, :datetime
   end
 end
