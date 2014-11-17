47dd0b28cf8386d71a764ca8ea23d28f40545029
Douglas Teixeira <teixeir3@gmail.com>
2014-11-17 15:06:51 -0500

    WIP: Updates some of german_cruising static page. Adds newrelic feature.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_signed_in!, only: [:new, :creat, :edit, :update, :sort, :destroy]
+  before_action :require_signed_in!, only: [:new, :creat, :edit, :update, :sort, :destroy ]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @promotion = @user.promotions.find(params[:inquiry_id]) if params[:inquiry_id]
+    @promotion = (@user.promotions.find(params[:inquiry_id]) if params[:inquiry_id]) || @user.promotions.build(title: params[:inquiry])
   end
   
   def create
