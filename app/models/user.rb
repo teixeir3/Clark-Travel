fb3d297bc1eea2202281ddf55289948fafc5f245
Douglas Teixeira <teixeir3@gmail.com>
2014-11-25 14:25:33 -0500

    Fixes bug in User#update_omniauth q

--- app/models/user.rb
+++ app/models/user.rb
@@ -96,7 +96,8 @@ class User < ActiveRecord::Base
   
   def update_omniauth!(auth)
     full_name = auth.info.name.split(" ")
-    self.update_attributes({provider: auth.provider, uid: auth.uid, first_name: full_name[0], last_name: full_name[-1], oauth_token: auth.credentials.token, oauth_expires_at: Time.at(auth.credentials.expires_at), fb_image_url: fb_picture_url})
+    self.oauth_token = auth.credentials.token
+    self.update_attributes({provider: auth.provider, uid: auth.uid, first_name: full_name[0], last_name: full_name[-1], oauth_expires_at: Time.at(auth.credentials.expires_at), fb_image_url: fb_picture_url})
   end
   
   def facebook
@@ -118,6 +119,7 @@ class User < ActiveRecord::Base
   def fb_picture_url
     facebook.get_picture("me")
   end
+  
   ### Auth Methods ###
   
   def is_admin?
