8a858e6c848b3aa20e3639950aefa6fc363e3918
Douglas Teixeira <teixeir3@gmail.com>
2014-12-01 14:06:52 -0500

    Changes UsersController#index to fetch users with display: true

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @users = User.all(order: "position")
+    @users = User.where(display: true).all(order: "position")
   end
   
   def new
